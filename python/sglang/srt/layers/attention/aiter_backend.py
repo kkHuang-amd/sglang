dcd2013f535235c512b259094b35b8958765c5dd
wunhuang <wunhuang@amd.com>
2025-04-30 01:50:43 +0000

    Fix shape mismatch problem after doing attn

--- python/sglang/srt/layers/attention/aiter_backend.py
+++ python/sglang/srt/layers/attention/aiter_backend.py
@@ -17,6 +17,7 @@ import triton.language as tl
 
 from sglang.global_config import global_config
 from sglang.srt.layers.attention.base_attn_backend import AttentionBackend
+from sglang.srt.layers.attention.utils import create_flashinfer_kv_indices_triton
 from sglang.srt.layers.dp_attention import get_attention_tp_size
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch, ForwardMode
 
@@ -25,11 +26,6 @@ if TYPE_CHECKING:
     from sglang.srt.model_executor.model_runner import ModelRunner
     from sglang.srt.speculative.spec_info import SpecInfo
     
-# flashinfer AMD fork
-from flashinfer import (
-    BatchPrefillWithPagedKVCacheWrapper, 
-)
-
 try:    
     from aiter import flash_attn_varlen_func
     from aiter import paged_attention_ragged
@@ -362,7 +358,7 @@ class AiterAttnBackend(AttentionBackend):
             alibi_slopes=None,
             return_lse=False,
             return_attn_probs=False,
-        )[0]
+        )
 
 
         return o.view(-1, layer.tp_q_head_num * layer.head_dim)
@@ -507,38 +503,3 @@ class AiterIndicesUpdaterPrefill:
 
         self.kv_indices = kv_indices
 
-@triton.jit
-def create_flashinfer_kv_indices_triton(
-    req_to_token_ptr,  # [max_batch, max_context_len]
-    req_pool_indices_ptr,
-    page_kernel_lens_ptr,
-    kv_indptr,
-    kv_start_idx,
-    kv_indices_ptr,
-    req_to_token_ptr_stride: tl.constexpr,
-):
-    BLOCK_SIZE: tl.constexpr = 512
-    pid = tl.program_id(axis=0)
-
-    req_pool_index = tl.load(req_pool_indices_ptr + pid)
-    kv_indices_offset = tl.load(kv_indptr + pid)
-
-    kv_start = 0
-    kv_end = 0
-    if kv_start_idx:
-        kv_start = tl.load(kv_start_idx + pid).to(tl.int32)
-        kv_end = kv_start
-    kv_end += tl.load(page_kernel_lens_ptr + pid).to(tl.int32)
-
-    num_loop = tl.cdiv(kv_end - kv_start, BLOCK_SIZE)
-    for i in range(num_loop):
-        offset = tl.arange(0, BLOCK_SIZE) + i * BLOCK_SIZE
-        mask = offset < kv_end - kv_start
-        data = tl.load(
-            req_to_token_ptr
-            + req_pool_index * req_to_token_ptr_stride
-            + kv_start
-            + offset,
-            mask=mask,
-        )
-        tl.store(kv_indices_ptr + kv_indices_offset + offset, data, mask=mask)
